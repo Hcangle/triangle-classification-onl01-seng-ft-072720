d770bffca0609e20378adec21e1039ae0ff9a161
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 19:24:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,11 +2,12 @@ class Triangle
   attr_accessor :side1, :side2, :side3 
   
   def initialize(side1, side2, side3)
-    @side1 = side1
+    @side1, @side2, @side3 = side1, side2, side3 
+    @sides = [side1, side2, side3]
   end 
   
-  def kind 
-    @equilateral
+  def kind
+    
     
     
   #class TriangleError < StandardError
