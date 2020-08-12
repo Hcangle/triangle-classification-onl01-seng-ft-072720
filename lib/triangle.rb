2b57eb559c8ac720fb0a50df6bedbb1c08c0cb7e
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 19:30:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,50 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3 
-  
+  attr_accessor :side1, :side2, :side3
+  @sides = []
+
   def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3 
-  end 
-  
+    @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
+  end
+
   def kind
-    if side1 == side2 && side2 = side3
+    if(negative? == true || valid? == false)
+      raise TriangleError
+    elsif(equilateral?)
       :equilateral
-    elsif side1 == side2 || side2 == side3 || side1 == side3 
-     :isosceles
-   else 
-     :scalene
-   end 
- end 
-    
-    
-    
-  #class TriangleError < StandardError
-  #  def message
-   #   "triangle kind is invalid."
-  #end
+    elsif(scalene?)
+      :scalene
+    elsif(isosceles?)
+      :isosceles
+    end
+  end
+
+  def negative?
+    @sides.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def equilateral?
+    (@side1 == @side2) && (@side1 == @side3)
+  end
+
+  def scalene?
+    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
+  end
+
+  def isosceles?
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
+  end
+
+  def valid?
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
